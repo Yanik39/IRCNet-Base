4ff761429cf6f6f9f75a44cf2fbded2c19f0455e
TemhAAhmeT <wmaster@mailbox.org>
2021-06-21 23:56:51 +0200

    gpg key server to ubuntu

--- base/scripts/unrealircd.sh
+++ base/scripts/unrealircd.sh
@@ -4,8 +4,8 @@ set -e
 
 cd /tmp
 wget https://www.unrealircd.org/downloads/unrealircd-$UNREAL_VERSION.tar.gz{,.asc}
-#gpg --keyserver keys.gnupg.net --recv-keys 0xA7A21B0A108FF4A9
-#gpg --verify unrealircd-$UNREAL_VERSION.tar.gz.asc unrealircd-$UNREAL_VERSION.tar.gz
+gpg --keyserver keyserver.ubuntu.com --recv-key 0xA7A21B0A108FF4A9
+gpg --verify unrealircd-$UNREAL_VERSION.tar.gz.asc unrealircd-$UNREAL_VERSION.tar.gz
 tar zxvf unrealircd-$UNREAL_VERSION.tar.gz -C /tmp
 cd /tmp/unrealircd-$UNREAL_VERSION
 expect /scripts/unrealircd.expect
