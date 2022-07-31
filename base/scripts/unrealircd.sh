47b97cd64d011f16b2c5d6013f99fe2154bfccf7
Yanik39 <12784047+Yanik39@users.noreply.github.com>
2022-07-31 14:50:54 +0200

    Update unrealircd.sh

--- base/scripts/unrealircd.sh
+++ base/scripts/unrealircd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e
-UNREAL_VERSION=$(curl -s https://www.unrealircd.org/downloads/list.json | jq -r '.[].Stable.version')
+UNREAL_VERSION=$(curl -s https://www.unrealircd.org/downloads/list.json | jq -r '.[].Stable.version | select( . != null )')
 
 cd /tmp
 wget https://www.unrealircd.org/downloads/unrealircd-$UNREAL_VERSION.tar.gz{,.asc}
-gpg --keyserver keyserver.ubuntu.com --recv-key 0xA7A21B0A108FF4A9
+#gpg --keyserver keyserver.ubuntu.com --recv-key 0xA7A21B0A108FF4A9
+wget -O- https://raw.githubusercontent.com/unrealircd/unrealircd/unreal60_dev/doc/KEYS | gpg --import
 gpg --verify unrealircd-$UNREAL_VERSION.tar.gz.asc unrealircd-$UNREAL_VERSION.tar.gz
 tar zxvf unrealircd-$UNREAL_VERSION.tar.gz -C /tmp
 cd /tmp/unrealircd-$UNREAL_VERSION
